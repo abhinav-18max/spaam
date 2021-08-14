826e98a29cc996b3413665ba294118ff376e64e0
abhinav-18max <75900799+abhinav-18max@users.noreply.github.com>
2021-08-14 16:26:13 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -259,5 +259,57 @@ for (var i = 0; i < pathEls.length; i++) {
   });
 }
 
+
+ new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+      }
+    }
+  });
+    
+  new swiper('shedule-slider',{
+      speed: 600,
+      loop:true,
+      autoplay:{
+          delay: 5000,
+          disableOnInteraction: false
+      },
+       slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+      }
+        
+      }
+  })
+
 })()
 
